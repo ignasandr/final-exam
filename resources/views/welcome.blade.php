b4c6b1daded148dca5a65b46958c6faddcd67cb6
Ignas Andriuškevičius <63236361+ignasandr@users.noreply.github.com>
2020-09-15 16:43:23 +0300

    Fixed welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,24 @@
 @section('content')
     <body>
         <div class="container-fluid text-center">
+
+
             <img src="/img/main.png" alt="horse site logo" class="img-fluid" width="300px">
-            <p>Welcome to client administration system. Please login. </p>
+            <!-- <p>Welcome to client administration system. Please login. </p> -->
+
+            <p>Welcome to client administration system.</p>
+            @if (Route::has('login'))
+                    @auth
+                        <a href="{{ url('/home') }}">Manage data</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
         </div>
     </body>
 @endsection
