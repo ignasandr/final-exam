798efbac7820eb82f752be2defc8f44066a61a5d
Ignas Andriuškevičius <63236361+ignasandr@users.noreply.github.com>
2020-09-15 16:53:09 +0300

    Image alt update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid text-center">
 
 
-            <img src="/img/main.png" alt="horse site logo" class="img-fluid" width="300px">
+            <img src="/img/main.png" alt="client system logo" class="img-fluid" width="300px">
             <!-- <p>Welcome to client administration system. Please login. </p> -->
 
             <p>Welcome to client administration system.</p>
